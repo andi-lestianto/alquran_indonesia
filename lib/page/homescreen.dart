df1f90144084814c2b8063e561f49ccc3886f46d
Andi Lestianto <andi7les@gmail.com>
2022-06-11 12:49:08 +0700

    Slider + selamat datang homepage

--- lib/page/homescreen.dart
+++ lib/page/homescreen.dart
@@ -1,5 +1,54 @@
 import 'package:alquran_indonesia/page/list_surat.dart';
+import 'package:alquran_indonesia/theme/theme.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+          child: Container(
+            color: Colors.amber,
+            child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                child: Stack(
+                  children: <Widget>[
+                    Image.network(item, fit: BoxFit.cover, width: 1000.0),
+                    Positioned(
+                      bottom: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(200, 0, 0, 0),
+                              Color.fromARGB(0, 0, 0, 0)
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                          ),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                        child: Text(
+                          'No. ${imgList.indexOf(item)} image',
+                          style: stylePoppins2.copyWith(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+        ))
+    .toList();
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -18,20 +67,36 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Stack(
             children: [
               Container(
+                padding: EdgeInsets.only(top: 50),
                 height: MediaQuery.of(context).size.height,
                 width: MediaQuery.of(context).size.width,
                 color: Color.fromARGB(255, 16, 154, 126),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
-                      height: 70,
-                      width: 70,
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 245, 245, 245)),
+                      margin: EdgeInsets.only(left: 20),
+                      height: 150,
+                      width: MediaQuery.of(context).size.width * 0.60,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Selamat Datang",
+                            style: stylePoppins1.copyWith(color: Colors.white),
+                          ),
+                          Text(
+                            "Lorem ipsum dolor si amet ipsum dolr sit amet Lorem ipsum dolor sit amet",
+                            maxLines: 4,
+                            style: stylePoppins2.copyWith(
+                                color: Colors.white.withOpacity(0.75)),
+                          ),
+                        ],
+                      ),
                     ),
                     Container(
+                      margin: EdgeInsets.only(right: 20),
                       height: 70,
                       width: 70,
                       decoration: BoxDecoration(
@@ -54,14 +119,14 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Column(
                     children: [
                       Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Container(
-                          width: MediaQuery.of(context).size.width * 1,
-                          height: 200,
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 16, 154, 126),
-                            borderRadius: BorderRadius.circular(25),
+                        padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                        child: CarouselSlider(
+                          options: CarouselOptions(
+                            autoPlay: true,
+                            aspectRatio: 2.0,
+                            enlargeCenterPage: true,
                           ),
+                          items: imageSliders,
                         ),
                       ),
                       Padding(
@@ -70,8 +135,8 @@ class _HomeScreenState extends State<HomeScreen> {
                             width: MediaQuery.of(context).size.width * 1,
                             child: Text(
                               "Silahkan Pilih Menu",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 15),
+                              style: stylePoppins2.copyWith(
+                                  fontWeight: FontWeight.bold),
                             )),
                       ),
                       Padding(
---
+++ lib/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle stylePoppins1 =
+    GoogleFonts.poppins().copyWith(fontWeight: FontWeight.w800, fontSize: 25);
+
+TextStyle stylePoppins2 =
+    GoogleFonts.poppins().copyWith(fontWeight: FontWeight.normal, fontSize: 15);
